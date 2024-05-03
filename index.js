b710fc41f2e6aa17c55dfd6dc6c5330bee55b6ea
Leith Green <leithgwrk22@gmail.com>
2024-05-03 14:10:10 +0100

    remove old code, replaced with mongoose and JWT authorisation
    Add JSON web Token authorisation, as well as removing older index.js code that has been re-written to work with mongoose

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const Users = Models.User;
 // BodyParser
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: true}));
+
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');  
+
+//returns static page
+app.use(express.static('public'));
+
+// morgan
+app.use(morgan('common'));
+
+// error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+
+});
+
 let users = [
   {
     id: 1,
@@ -138,31 +157,16 @@ let movies = [
   } 
 ];
 
-// GET requests
- app.get('/', (req, res) => {
-  res.send('View my top 10 favorite movies');
-});
-
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
-
-app.get('/movies', (req, res) => {
-  res.json(movies);
-});
-
-//returns static page
-app.use(express.static('public'));
-
-// morgan
-app.use(morgan('common'));
-
-
-// error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-
+// JWT Authentication
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 //Add a user
@@ -265,110 +269,6 @@ app.delete('/users/:Username', async (req, res) => {
   });
 });
 
-//UPDATE
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-  
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('No such user')
-  }
-
-})
-
-//CREATE
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been sent to user ${id}'s array`);
-  } else {
-    res.status(400).send('No such user')
-  }
-
-})
-
-//DELETE
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    user.favoriteMovies = user.favorirteMovies.filter( title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-  } else {
-    res.status(400).send('No such user')
-  }
-
-})
-
-//DELETE
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  
-  let user = users.find( user => user.id == id );
-
-  if (user) {
-    users = users.filter( user => user.id != id);
-    res.status(200).send(`User ${id} has been deleted`);
-  } else {
-    res.status(400).send('No such user')
-  }
-
-})
-
-//READ 
-app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
-})
-
-//READ 
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('No such movie')
-  }
-
-})
-
-//READ 
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('No such genre')
-  }
-
-})
-
-//READ 
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.Director.Name === directorName).Director;
-
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('No such director')
-  }
-
-})
 
 // listen for requests
 app.listen(8080, () => console.log('Your app is listening on port 8080.'))
