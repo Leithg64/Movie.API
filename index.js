1ab1b67abfcbf4e146f7005592f3a7e3bc7cbe84
Leith Green <leithgwrk22@gmail.com>
2024-05-07 19:02:19 +0100

    secure connection uri with environment variables
    secure connection uri with environment variables

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 // BodyParser
 app.use(bodyParser.json());
