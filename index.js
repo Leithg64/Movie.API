dd202e30dd73fd38f53e5492aa5e2c0b8bf22315
Leith Green <leithgwrk22@gmail.com>
2024-05-08 17:11:16 +0100

    add .env to gitignore
    add .env to gitignore

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
